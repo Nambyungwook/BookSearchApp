4c4779d306a9a56b8ef203f0200e2afaec16392f
Nambyungwook <skaquddnr21@gmail.com>
2022-09-28 11:49:48 +0900

    초기 UI 구현

--- app/src/main/java/com/nbw/booksearchapp/MainActivity.kt
+++ app/src/main/java/com/nbw/booksearchapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.nbw.booksearchapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nbw.booksearchapp.databinding.ActivityMainBinding
+import com.nbw.booksearchapp.ui.view.FavoriteFragment
+import com.nbw.booksearchapp.ui.view.SearchFragment
+import com.nbw.booksearchapp.ui.view.SettingFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupBottomNavigationView()
+        // 앱이 처음 실행되었을때 SearchFragment가 실행되도록 설정
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_search
+        }
+    }
+
+    private fun setupBottomNavigationView() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.fragment_search -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_favorite -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, FavoriteFragment())
+                        .commit()
+                    true
+                }
+                R.id.fragment_setting -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frame_layout, SettingFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
